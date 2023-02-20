50efe652da89ae4290a6dff05e209037427f042d
Pau Fernandez <pau.fernandez@upc.edu>
2023-02-20 20:59:23 +0100

    Añadimos botón para pasar página
    Necesitamos poner 'page' como dependencia de useEffect

--- 2023-02-20 - React 2/react-2/src/components/BeerList.jsx
+++ 2023-02-20 - React 2/react-2/src/components/BeerList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Beer from './Beer';
+import Beer from "./Beer";
 
 const testBeerList = [
   {
@@ -15,24 +15,36 @@ const testBeerList = [
 ];
 
 const BeerList = () => {
+  const [page, setPage] = useState(1);
   const [beerList, setBeerList] = useState(null);
 
+  const nextPage = () => setPage(c => c + 1);
+
   const loadBeers = async () => {
-    const response = await fetch(`https://api.punkapi.com/v2/beers`);
+    const response = await fetch(
+      `https://api.punkapi.com/v2/beers?page=${page}&per_page=10`
+    );
     const loadedBeerList = await response.json();
     setBeerList(loadedBeerList);
-  }
+  };
 
   useEffect(() => {
     loadBeers();
-  }, []);
+  }, [page]);
 
   if (beerList === null) {
     return <div>Loading...</div>;
   }
-  return <ol className="beer-list">
-    {beerList.map(beer => <Beer beer={beer} />)}
-  </ol>;
+  return (
+    <>
+      <button onClick={nextPage}>Next Page ({page})</button>
+      <ol className="beer-list">
+        {beerList.map((beer) => (
+          <Beer beer={beer} />
+        ))}
+      </ol>
+    </>
+  );
 };
 
 export default BeerList;
