432fd64c2398969492c9ec85435f89e8f1e546c6
Pau Fernandez <pau.fernandez@upc.edu>
2023-02-27 20:36:27 +0100

    Ponemos el <header> con dos links

--- 2023-02-27 - React 3/react-3/src/App.jsx
+++ 2023-02-27 - React 3/react-3/src/App.jsx
@@ -1,17 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink } from "react-router-dom";
 import "./App.css";
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import BeerDetailsPage from './pages/BeerDetailsPage';
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import BeerDetailsPage from "./pages/BeerDetailsPage";
 
 // Los componentes son funciones *puras*
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/search" element={<SearchPage />} />
-    <Route path="/beers/:id" element={<BeerDetailsPage />} />
-  </Routes>
+  return (
+    <main>
+      <header>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/search">Search</NavLink>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/beers/:id" element={<BeerDetailsPage />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
