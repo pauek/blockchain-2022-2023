f636524379e478d0ca46142c447f4d3e54e432d8
Pau Fernandez <pau.fernandez@upc.edu>
2023-02-27 21:19:03 +0100

    Acabar BeerDetails

--- 2023-02-27 - React 3/react-3/src/pages/BeerDetailsPage.jsx
+++ 2023-02-27 - React 3/react-3/src/pages/BeerDetailsPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import './BeerDetailsPage.css';
+import { loadBeer } from '../api';
 
 const BeerDetailsPage = () => {
-  return (
-    <div>BeerDetailsPage</div>
-  )
-}
+  const { id } = useParams();
+  const [beer, setBeer] = useState(null);
 
-export default BeerDetailsPage
+  useEffect(() => {
+    loadBeer(id).then(setBeer);
+  }, [id]);
+
+  if (beer === null) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="beer-details">
+      <img src={beer.image_url} alt="Beer picture" />
+      <div className="info">
+        <h1>{beer.name}</h1>
+        <h3>{beer.tagline}</h3>
+        <p>{beer.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BeerDetailsPage;
