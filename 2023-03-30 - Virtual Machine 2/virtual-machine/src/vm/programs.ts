ac5705890a2e0e67e912f819764b51e5c768f8d5
Pau Fernandez <pau.fernandez@upc.edu>
2023-03-30 20:34:18 +0200

    Reformateo programas

--- 2023-03-30 - Virtual Machine 2/virtual-machine/src/vm/programs.ts
+++ 2023-03-30 - Virtual Machine 2/virtual-machine/src/vm/programs.ts
@@ -1,55 +1,55 @@
 
 export const testMod = `
-push 5
-push 17
-mod
-output
-halt
+  push 5
+  push 17
+  mod
+  output
+  halt
 `;
 
 export const testMayor = `
-push 7
-push 3
-gt
-output
-halt
+  push 7
+  push 3
+  gt
+  output
+  halt
 `
 
 export const testXor = `
-push true
-push true
-xor
-output
-halt
+  push true
+  push true
+  xor
+  output
+  halt
 `
 
 export const testIf1 = `
-push true
-push 9
-JUMPI
-push 12345
-output
-halt
-push 99999
-output
-halt
+  push true
+  push 9
+  jumpi
+  push 12345
+  output
+  halt
+  push 99999
+  output
+  halt
 `
 
 export const testLoop1 = `
-    push 100
-    push 1
-start:
-    dup2
-    dup2
-    gt
-    push end
-    jumpi
-    dup1
-    output
-    push 1
-    add
-    push start
-    jump
-end:
-    halt
+  push 100
+  push 1
+START:
+  dup2
+  dup2
+  gt
+  push END
+  jumpi
+  dup1
+  output
+  push 1
+  add
+  push START
+  jump
+END:
+  halt
 `
